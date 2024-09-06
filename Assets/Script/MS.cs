9824f45a6a5fc14fce27fd828fd514ab3540e1e4
Dennytan Raharjo <113045830+MiracLeDen@users.noreply.github.com>
2024-09-06 11:40:53 +0700

    Tugas Week 5

--- Assets/Script/MS.cs
+++ Assets/Script/MS.cs
@@ -6,89 +6,77 @@ using UnityEngine.SceneManagement;
 
 public class MS : MonoBehaviour
 {
-    Rigidbody2D rb;
-    int speed;
-    float jumpForce = 100;
-    private bool isGrounded;
-    public Button jumpButton;
+    private Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
 
+    public Animator animasi;
+    public bool isLeft;
+    public bool isGrounded;
+    public GameObject fireball;
+    public GameObject firepos;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        speed = 3;
-
-        
-        if (jumpButton != null)
-        {
-            jumpButton.onClick.AddListener(Jump);
-        }
     }
-
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.tag == "Pindah1")
-        {
-            SceneManager.LoadScene("Scene3");
-        }
-        if (col.tag == "Pindah2")
-        {
-            SceneManager.LoadScene("Scene2");
-        }
-        if (col.tag == "Pindah3")
-        {
+    private void OnTriggerEnter2D(Collider2D col){
+        if(col.tag == "Pindah3"){
             SceneManager.LoadScene("Scene4");
         }
+        if(col.tag == "Pindah1"){
+            SceneManager.LoadScene("Scene3");
+        }
+        if(col.tag == "Pindah2"){
+            SceneManager.LoadScene("Scene2");
+        }
     }
-
     void Update()
     {
         Move();
+        jump();
+        Attack();
     }
-    public void Jump()
-    {
-        if (isGrounded)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
-    }
-    public void Move(){
-                rb.velocity = new Vector2(0, 0);
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 1.1f);
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.velocity = new Vector2(speed, 0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.velocity = new Vector2(-speed, 0);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            rb.velocity = new Vector2(0, speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.velocity = new Vector2(0, -speed);
-        }
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
-        {
-            rb.velocity = new Vector2(-speed, speed);
-        }
-        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
-        {
-            rb.velocity = new Vector2(speed, speed);
-        }
-        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
-        {
-            rb.velocity = new Vector2(speed, -speed);
-        }
-        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
-        {
-            rb.velocity = new Vector2(-speed, -speed);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
+    void Attack(){
+        if(Input.GetMouseButtonDown(0)){
+            StartCoroutine(DelayStrike());
         }
     }
+    IEnumerator DelayStrike(){
+        yield return new WaitForSeconds(0.2f);
+        GameObject spawnedFire = (GameObject)Instantiate(fireball);
+        spawnedFire.transform.position = firepos.transform.position;
+    }
+    void Move(){
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        
+        if(moveInput!=0){animasi.SetBool("IsWalking",true);}
+        else animasi.SetBool("IsWalking",false);
+
+        if(moveInput > 0){
+            transform.localScale = new Vector3(1,1,1);
+            isLeft = false;
+        }
+        else if(moveInput < 0){
+            transform.localScale = new Vector3(-1,1,1);
+            isLeft = true;
+        }
+    }
+
+    void jump(){
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.collider.CompareTag("Ground")){
+            isGrounded = true;
+        }    
+    }
+    void OnCollisionExit2D(Collision2D collision){
+        if(collision.collider.CompareTag("Ground")){
+            isGrounded = true;
+        }
+}
 }
